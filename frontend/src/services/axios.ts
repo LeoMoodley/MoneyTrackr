a9a9a3c8bbfbc93c8c902072980ddd6d5bec798d
Brandon <126841628+BrandonTran03@users.noreply.github.com>
2025-04-24 21:11:56 -0400

    Update axios.ts

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -2,7 +2,7 @@ import axios, { InternalAxiosRequestConfig, AxiosError } from "axios";
 
 // Create an axios instance
 const instance = axios.create({
-  baseURL: "http://18.222.161.63:8000/api/",
+  baseURL: "https://3.15.229.131:8000/api/",
 });
 
 let isRefreshing = false;
